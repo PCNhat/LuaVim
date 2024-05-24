f045b5369747cb754b9bb4ee9adf761d1b68034b
NhatPC <nhat.pcn@gmail.com>
2024-05-25 00:09:09 +0700

    Update colorscheme

--- lua/nhatpc/plugins/colorscheme.lua
+++ lua/nhatpc/plugins/colorscheme.lua
@@ -14,9 +14,25 @@ return {
                 comments = { italic = false },
                 keywords = { italic = false },
                 -- Background styles. Can be "dark", "transparent" or "normal"
-                sidebars = "dark", -- style for sidebars, see below
+                sidebars = "normal", -- style for sidebars, see below
                 floats = "dark", -- style for floating windows
             },
+
+            --- You can override specific color groups to use other groups or a hex color
+            --- function will be called with a ColorScheme table
+            ---@param colors ColorScheme
+            on_colors = function(colors)
+                colors.bg = "#16161e"
+            end,
+
+            --- You can override specific highlights to use other groups or a hex color
+            --- function will be called with a Highlights and ColorScheme table
+            ---@param highlights Highlights
+            ---@param colors ColorScheme
+            on_highlights = function(highlights, colors)
+                Special = { fg = colors.magenta } -- (preferred) any special symbol
+            end,
+            
         })
         vim.cmd("colorscheme tokyonight")
     end
