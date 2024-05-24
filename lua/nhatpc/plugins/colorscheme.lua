9a970cc3007071761da61c0a540d7fac30b70195
PCNhat <nhatpc@nissho-vn.com>
2024-05-24 13:21:17 +0700

    Colorscheme, Neotree

---
+++ lua/nhatpc/plugins/colorscheme.lua
@@ -0,0 +1,23 @@
+return {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+	config = function()
+        require("tokyonight").setup({
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            style = "night", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+            styles = {
+                -- Style to be applied to different syntax groups
+                -- Value is any valid attr-list value for `:help nvim_set_hl`
+                comments = { italic = false },
+                keywords = { italic = false },
+                -- Background styles. Can be "dark", "transparent" or "normal"
+                sidebars = "dark", -- style for sidebars, see below
+                floats = "dark", -- style for floating windows
+            },
+        })
+        vim.cmd("colorscheme tokyonight")
+    end
+}
