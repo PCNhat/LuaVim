6e60806a0c907f53bfb4fcd8724f5ca1633e0280
NhatPC <nhat.pcn@gmail.com>
2024-05-25 22:00:58 +0700

    Add desc to keymaps

--- lua/nhatpc/plugins/whichkey.lua
+++ lua/nhatpc/plugins/whichkey.lua
@@ -4,10 +4,22 @@ return {
     init = function()
         vim.o.timeout = true
         vim.o.timeoutlen = 300
+
+        local wk = require("which-key")
+        wk.register(
+          {
+            -- add group
+            ["<leader>"] = {
+              f = { name = "Find" },
+              s = { name = "Search" },
+            }
+          }
+        )
     end,
+
     opts = {
     -- your configuration comes here
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
-    }
+    },
 }
