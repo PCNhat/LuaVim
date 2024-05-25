ee2826ef92ffe372fd00477b3761e032c08a69df
NhatPC <nhat.pcn@gmail.com>
2024-05-25 23:18:10 +0700

    [Telescope] Add keymaps

--- lua/nhatpc/plugins/telescope.lua
+++ lua/nhatpc/plugins/telescope.lua
@@ -11,7 +11,25 @@ return {
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = '[F]ind [F]iles' })
         vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = '[F]ind [B]uffers' })
-        vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch Live [G]grep (root)' })
+        vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+        vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by Live [G]grep (root)' })
         vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp tags' })
+        vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
+        vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+
+        vim.keymap.set('n', '<leader>/', function()
+                -- You can pass additional configuration to Telescope to change the theme, layout, etc.
+                builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+                    winblend = 0,
+                    previewer = false,
+                    fuzzy_find = false,
+                    layout_config = {
+                        width = 0.8,
+                        height = 0.9,
+                    }
+                })
+            end,
+            { desc = '[/] Fuzzily search in current buffer' }
+        )
     end
 }
