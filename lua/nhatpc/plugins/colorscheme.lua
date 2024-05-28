a51e5583e2b131c640ff41b86cfd71d36b770728
PCNhat <nhatpc@nissho-vn.com>
2024-05-28 16:24:43 +0700

    [colorscheme]Change background to transperant

--- lua/nhatpc/plugins/colorscheme.lua
+++ lua/nhatpc/plugins/colorscheme.lua
@@ -8,13 +8,14 @@ return {
             -- your configuration comes here
             -- or leave it empty to use the default settings
             style = "night", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+            transparent = true,
             styles = {
                 -- Style to be applied to different syntax groups
                 -- Value is any valid attr-list value for `:help nvim_set_hl`
                 comments = { italic = false },
                 keywords = { italic = false },
                 -- Background styles. Can be "dark", "transparent" or "normal"
-                sidebars = "normal", -- style for sidebars, see below
+                sidebars = "transparent", -- style for sidebars, see below
                 floats = "normal", -- style for floating windows
             },
 
