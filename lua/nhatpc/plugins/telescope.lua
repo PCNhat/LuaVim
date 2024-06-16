3ec335ff68d5361db5898cea0546b049875b48ed
NhatPC <nhat.pcn@gmail.com>
2024-06-16 15:49:30 +0700

    [telescope]Update UI for file_find drop down

--- lua/nhatpc/plugins/telescope.lua
+++ lua/nhatpc/plugins/telescope.lua
@@ -9,7 +9,16 @@ return {
         require('telescope').setup({})
 
         local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = '[F]ind [F]iles' })
+        vim.keymap.set('n', '<leader>ff', function ()
+                builtin.find_files(require('telescope.themes').get_dropdown({
+                    layout_config = {
+                        width = 0.8,
+                        height = 0.9,
+                    },
+                }))
+            end ,
+            { desc = '[F]ind [F]iles' }
+        )
         vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = '[F]ind [B]uffers' })
         vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
         vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by Live [G]grep (root)' })
