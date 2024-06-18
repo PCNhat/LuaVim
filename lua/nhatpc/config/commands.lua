1da8351bba45354a65adf70666425a27accb5a19
PCNhat <nhat.pcn@gmail.com>
2024-06-18 14:05:12 +0700

    [commands]Highlight yanked text

--- lua/nhatpc/config/commands.lua
+++ lua/nhatpc/config/commands.lua
@@ -1,3 +1,11 @@
+-- Highlight yanked text for 200ms using the "Visual" highlight group
+vim.cmd[[
+augroup highlight_yank
+autocmd!
+au TextYankPost * silent! lua vim.highlight.on_yank({higroup="Visual", timeout=200})
+augroup END
+]]
+
 -- Copy absolute path
 vim.api.nvim_create_user_command("CopyPath", function()
     local path = vim.fn.expand("%:p")
