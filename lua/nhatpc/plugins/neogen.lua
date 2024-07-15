a844644f74ca78f30f8f6249422083b8268148af
PCNhat <nhat.pcn@gmail.com>
2024-07-15 16:17:36 +0700

    [neogen] Add shortcuts

--- lua/nhatpc/plugins/neogen.lua
+++ lua/nhatpc/plugins/neogen.lua
@@ -3,8 +3,9 @@ return {
     tag = "2.17.1",
     config = function ()
         local neogen = require("neogen")
-        -- vim.keymap.set('i', '<C-l>', neogen.jump_next)
-        -- vim.keymap.set('i', '<C-h>', neogen.jump_prev)
+        vim.keymap.set("n", "<Leader>ng", neogen.generate, { desc = "[N]eogen [G]enerate" })
+        vim.keymap.set('i', '<C-l>', neogen.jump_next, { desc = "Neogen jump next" })
+        vim.keymap.set('i', '<C-h>', neogen.jump_prev, { desc = "Neogen jump previous" })
 
         neogen.setup()
     end
