dcc8c4f8b092d378d3ea20526b782fd01b63efcc
PCNhat <nhat.pcn@gmail.com>
2024-11-26 15:25:15 +0700

    [Neotree]Deleted because no longer used

--- lua/nhatpc/plugins/neotree.lua
+++ lua/nhatpc/plugins/neotree.lua
@@ -1,17 +1,17 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    },
-    config = function ()
-        local stats = vim.uv.fs_stat(vim.fn.argv(0))
-        if stats and stats.type == "directory" then
-          -- vim.cmd("Neotree filesystem reveal left")
-          --vim.cmd("Neotree position=current")
-        end
-    end
+    -- "nvim-neo-tree/neo-tree.nvim",
+    -- branch = "v3.x",
+    -- dependencies = {
+    --   "nvim-lua/plenary.nvim",
+    --   "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    --   "MunifTanjim/nui.nvim",
+    --   -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    -- },
+    -- config = function ()
+    --     local stats = vim.uv.fs_stat(vim.fn.argv(0))
+    --     if stats and stats.type == "directory" then
+    --       -- vim.cmd("Neotree filesystem reveal left")
+    --       --vim.cmd("Neotree position=current")
+    --     end
+    -- end
 }
